b9db28f513ab3f687be48f5704e699ce4147ebb8
Calvin Golas <32349728+CalvinGolas@users.noreply.github.com>
2018-11-27 11:05:55 -0700

    Initialized bash and c files

---
+++ fac.c
@@ -0,0 +1,14 @@
+extern int factorial(int n);
+int factorial_inefficient(int n){
+		int counter = n;
+		int result = 1;
+		while(counter > 1){
+			result = result * counter;
+			counter--;
+		}
+		return result;
+}
+
+int main(){
+	return factorial_inefficient(5);
+}
