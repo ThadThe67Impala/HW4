e123900c6bb2f442839a40e7a5f4e46d0cd63384
CalvinGolas <calvingolas@gmail.com>
2018-11-29 11:48:21 -0700

    updated bash script

--- fac.s
+++ fac.s
@@ -22,12 +22,12 @@ factorial_inefficient:
 	@ frame_needed = 1, uses_anonymous_args = 0
 	@ link register save eliminated.
 	str	fp, [sp, #-4]!
-	mov 	fp, sp
+	add	fp, sp, #0
 	sub	sp, sp, #20
 	str	r0, [fp, #-16]
 	ldr	r3, [fp, #-16]
 	str	r3, [fp, #-8]
-	mov 	r3, #1
+	mov	r3, #1
 	str	r3, [fp, #-12]
 	b	.L2
 .L3:
@@ -42,10 +42,9 @@ factorial_inefficient:
 	ldr	r3, [fp, #-8]
 	cmp	r3, #1
 	bgt	.L3
-	ldr	r0, [fp, #-12]
-	//mov	r0, r3
-	//add	sp, fp, #0
-	mov 	sp, fp
+	ldr	r3, [fp, #-12]
+	mov	r0, r3
+	add	sp, fp, #0
 	@ sp needed
 	ldr	fp, [sp], #4
 	bx	lr
@@ -63,8 +62,8 @@ main:
 	add	fp, sp, #4
 	mov	r0, #5
 	bl	factorial_inefficient
-	//mov	r3, r0
-	//mov	r0, r3
+	mov	r3, r0
+	mov	r0, r3
 	pop	{fp, pc}
 	.size	main, .-main
 	.ident	"GCC: (Raspbian 6.3.0-18+rpi1+deb9u1) 6.3.0 20170516"
--- hw4.sh
+++ hw4.sh
@@ -8,4 +8,12 @@ as -o fac.o fac.s
 
 gcc -o exec1 fac.o
 
-gcc -o exec2 factorial.o  
+gcc -o exec2 factorial.o 
+
+./exec2
+
+echo $?
+
+./exec1
+
+echo $?
