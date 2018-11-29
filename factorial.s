9b8f24739eb01e56cbe8c674469d3cfeb4cb5048
CalvinGolas <calvingolas@gmail.com>
2018-11-29 11:42:34 -0700

    implemented instructions

--- factorial.s
+++ factorial.s
@@ -1,8 +1,24 @@
+
 	.global f
-
 f:
+.loop:	
 	
+	cmp 	r2, #1
+	ITTT	NE	
+	subNE	r2, r2, #1
+	mulNE 	r0, r2	
+	bNE	.loop
+	b	.over	
 
-	mul	r0, r1, r2
+	.global main
+main:
+	
+	mov	r0, #5
+	mov	r2, r0
+	
+	cmp 	r2, #1
+	bNE	f
+.over: 
+	mov 	r0, r0
+	bx	lr	
 
-	bx	lr
